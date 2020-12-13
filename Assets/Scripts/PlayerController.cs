c216edcd86b6b174fe20c8512b80b955444d2e57
Wills-M <wmm38@drexel.edu>
2020-12-13 18:54:54 -0500

    Added very simple player controller to allow click movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.destination = hit.point;
+                Debug.Log(hit.point);
+            }
+        }
+    }
+
+}
