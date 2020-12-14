fd744d0131cb8cdf1b7538bb9a2cdfd49c5d4757
Wills-M <wmm38@drexel.edu>
2020-12-13 19:06:49 -0500

    Camera now follows player as they move.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform followingCamera;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        SetOffset();
+    }
+
+    private void Update()
+    {
+        followingCamera.position = transform.position + offset;
+    }
+
+    private void SetOffset()
+    {
+        offset = followingCamera.position - transform.position;
+    }
+}
